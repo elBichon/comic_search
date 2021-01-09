38bb05db6b4af2c33233e43ace534f1d61878038
elBichon <reuglewiczjeanedouard@gmail.com>
2021-01-10 00:05:36 +0100

    Update test_comic.py

--- site/test_comic.py
+++ site/test_comic.py
@@ -12,21 +12,12 @@ from nltk.stem.snowball import SnowballStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-def test_clean_data1():
-	a = utils.clean_data('batman this is the Killing Joke,;.:/.123')
-	b = 'batman killing joke'
-	assert a == b
 
 def test_clean_data2():
 	assert utils.clean_data('') == False
 
 def test_clean_data3():
 	assert utils.clean_data(1) == False
-
-def test_read_data1():
-	a = utils.read_data('final_clean_prod4.csv').shape
-	b = (16205, 10)
-	assert a == b
 
 def test_read_data2():
 	assert utils.read_data('') == False
