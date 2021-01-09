723c5e85aa0fa4bce29b8b4d98413010c7f7b1c2
elBichon <reuglewiczjeanedouard@gmail.com>
2021-01-10 00:01:14 +0100

    Update test_comic.py

--- site/test_comic.py
+++ site/test_comic.py
@@ -13,7 +13,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 def test_clean_data1():
-	assert utils.clean_data('batman this is the Killing Joke,;.:/.123') == 'batman killing joke'
+	a = utils.clean_data('batman this is the Killing Joke,;.:/.123')
+	b = 'batman killing joke'
+	assert a == b
 
 def test_clean_data2():
 	assert utils.clean_data('') == False
@@ -22,11 +24,12 @@ def test_clean_data3():
 	assert utils.clean_data(1) == False
 
 def test_read_data1():
-	assert utils.read_data('final_clean_prod4.csv').shape == (16205, 10)
+	a = utils.read_data('final_clean_prod4.csv').shape
+	b = (16205, 10)
+	assert a == b
 
 def test_read_data2():
 	assert utils.read_data('') == False
 
 def test_read_data3():
 	assert utils.read_data(4) == False
-
